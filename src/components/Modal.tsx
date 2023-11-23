fdf72c471c6d4050957ea275ad1cf66a657543d2
WanG <69587456+x-wang-x@users.noreply.github.com>
2023-11-24 01:03:24 +0700

    Working on add function

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,30 @@
 import Button from "./Button";
-
+import { setData } from "../module/storeData";
 interface props {
   id: string;
   closeModalFunction: () => void;
   value: {
     name: string;
   };
+}
+const add = (title : string,group_id: string,content: string,id : number) => {
+  setData('index',{
+    group_id : group_id,
+    content_id : id,
+    title : title
+  })
+  setData('data',{
+    content_id : id,
+    content : content
+  })
+  alert("Done")
+}
+const getAdd = () => {
+  const title = document.getElementById('name') as HTMLInputElement
+  const content = document.getElementById('txt') as HTMLTextAreaElement
+  const group_idx : HTMLSelectElement = document.getElementById('sbox') as HTMLSelectElement
+  
+  add(title.value,group_idx.value,content.value,5) 
 }
 const Modal = ({ id, closeModalFunction, value }: props) => {
   return (
@@ -19,50 +38,56 @@ const Modal = ({ id, closeModalFunction, value }: props) => {
       >
         <div className="flex flex-col p-4 w-full h-full">
           {/* Modal header */}
-            <div className="flex-none flex justify-between pb-1 mb-3 border-b-2 dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Add New Notes
-              </h3>
-              <button
-                onClick={closeModalFunction}
-                type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-toggle={id}
+          <div className="flex-none flex justify-between pb-1 mb-3 border-b-2 dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Add New Notes
+            </h3>
+            <button
+              onClick={closeModalFunction}
+              type="button"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-toggle={id}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            {/*  Modal body  */}
-            <div className="grow flex flex-col">
-              <span className="mb-1">Note Name : </span>
-              <input
-                type="text"
-                className="h-fit mb-3 pl-2 py-1 border border-wx-light bg-wx-dark text-wx-light focus:outline-none"
-                defaultValue={value?.name}
-              ></input>
-              <span className="mb-1">Write Your Notes : </span>
-              <textarea className="h-full resize-none overflow-scroll mb-3 pl-2 border border-wx-light bg-wx-dark text-wx-light focus:outline-none"></textarea>
-            </div>
-            {/* <!-- Modal footer --> */}
-            <div className="flex-none mt-3 pt-1 flex items-end border-t-2 border-wx-light justify-end gap-2">
-              <Button onClick={() => alert("ok")} text="Add" />
-              <Button onClick={() => closeModalFunction()} text="Cancel" />
-            </div>
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+          {/*  Modal body  */}
+          <div className="grow flex flex-col">
+            <span className="mb-1">Note Name : </span>
+            <input
+            id="name"
+              type="text"
+              className="h-fit mb-3 pl-2 py-1 border border-wx-light bg-wx-dark text-wx-light focus:outline-none"
+              defaultValue={value?.name}
+            ></input>
+            <span className="mb-1">Group : </span>
+            <select id="sbox" className="bg-wx-dark border border-wx-light py-1 mb-3 pl-2">
+              <option value={1}>Group 1</option>
+              <option value={2}>Group 2</option>
+            </select>
+            <span className="mb-1">Write Your Notes : </span>
+            <textarea id="txt" className="h-full resize-none overflow-scroll mb-3 pl-2 border border-wx-light bg-wx-dark text-wx-light focus:outline-none"></textarea>
+          </div>
+          {/* <!-- Modal footer --> */}
+          <div className="flex-none mt-3 pt-1 flex items-end border-t-2 border-wx-light justify-end gap-2">
+            <Button onClick={() => getAdd()} text="Add" />
+            <Button onClick={() => closeModalFunction()} text="Cancel" />
           </div>
         </div>
+      </div>
     </>
   );
 };
--- src/module/storeData.ts
+++ src/module/storeData.ts
@@ -1,15 +1,22 @@
 const getData = (key: string): [] => {
   return JSON.parse(localStorage.getItem(key) || "{}");
 };
-const setData = (key: string, item: string) => {
-  localStorage.setItem(key, item);
+const setData = (key: string, item: object | string) => {
+  const datas = getData(key);
+  if (datas.length == undefined) {
+    localStorage.setItem(key, JSON.stringify([item]));
+
+  } else {
+    datas.push(item as never);
+    localStorage.setItem(key, JSON.stringify(datas));
+  }
 };
 const search = (key: string, name = "", query: string): [] => {
   const data = getData(key);
   const res: [] = [];
   data.map((item) => {
-    if (item[name]==query){
-      res.push(item)
+    if (item[name] == query) {
+      res.push(item);
     }
   });
   return res;
